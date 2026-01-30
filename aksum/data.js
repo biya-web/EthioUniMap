39cbea7f1b997b6038de2f23edb563e2b57e326b
biya-web <biniambrhane611@gmail.com>
2026-01-30 08:24:43 -0900

    Add campus data for Aksum University
    Added campus data for Aksum University including building details, coordinates, and types.

--- aksum/data.js
+++ aksum/data.js
@@ -1,1 +1,91 @@
-
+const campusData = {
+    name: "Aksum University (Main Campus)",
+    center: [14.106, 38.709], // Centered near the dorms/admin area
+    zoom: 17,
+    buildings: [
+        {
+            id: "20",
+            name: "Building 20",
+            type: "class", 
+            coords: [[14.108478, 38.711255], [14.108935, 38.711507], [14.109232, 38.710750], [14.109008, 38.710616]],
+            details: "<b>Building 20 Details</b><br>Main conference hall."
+        },
+        {
+            id: "21",
+            name: "Building 21",
+            type: "admin",
+            coords: [[14.109388, 38.709211], [14.109690, 38.708910], [14.109471, 38.708626], [14.109097, 38.708819]],
+            details: "<b>Building 21 Details</b><br>Digital Library."
+        },
+        {
+            id: "32",
+            name: "Building 32",
+            type: "dorm",
+            coords: [[14.106620, 38.707779], [14.106579, 38.707526], [14.106240, 38.707532], [14.106277, 38.707848]],
+            details: "<b>Building 32 Details</b><br>Male Dormitory."
+        },
+        {
+            id: "42",
+            name: "Building 42",
+            type: "dorm",
+            coords: [[14.106126, 38.707870], [14.105798, 38.707902], [14.105767, 38.707612], [14.106074, 38.707575]],
+            details: "<b>Building 42 Details</b><br>Male Dormitory."
+        },
+        {
+            id: "31",
+            name: "Block 31",
+            type: "dorm",
+            coords: [[14.106651, 38.707339], [14.106969, 38.707296], [14.106932, 38.707054], [14.106615, 38.707103]],
+            details: "<b>Block 31 Details</b><br>Male Dormitory."
+        },
+        {
+            id: "84",
+            name: "Block 84",
+            type: "admin",
+            coords: [[14.107926, 38.711845], [14.107723, 38.712134], [14.107609, 38.712075], [14.107796, 38.711770]],
+            details: "<b>Block 84 Details</b><br>Registrarial Office."
+        },
+        {
+            id: "76",
+            name: "Block 76",
+            type: "class",
+            coords: [[14.103696, 38.709790], [14.103379, 38.709849], [14.103343, 38.709613], [14.103670, 38.709543]],
+            details: "<b>Block 76 Details</b><br>Lecture Classrooms."
+        },
+        {
+            id: "77",
+            name: "Block 77",
+            type: "class",
+            coords: [[14.103696, 38.710021], [14.103400, 38.710075], [14.103426, 38.710289], [14.103728, 38.710262]],
+            details: "<b>Block 77 Details</b><br>Lecture Classrooms."
+        },
+        {
+            id: "78",
+            name: "Block 78",
+            type: "class",
+            coords: [[14.103551, 38.709125], [14.103275, 38.709173], [14.103228, 38.708937], [14.103514, 38.708884]],
+            details: "<b>Block 78 Details</b><br>Lecture Classrooms."
+        },
+        {
+            id: "59",
+            name: "Block 59",
+            type: "art",
+            coords: [[14.105944, 38.709586], [14.105772, 38.709613], [14.105705, 38.709195], [14.105871, 38.709157]],
+            details: "<b>Block 59 Details</b><br>Art Center."
+        },
+        {
+            id: "58",
+            name: "Block 58",
+            type: "class",
+            coords: [[14.106100, 38.709533], [14.106246, 38.709517], [14.106178, 38.709141], [14.106074, 38.709130]],
+            details: "<b>Block 58 Details</b><br>ECE Department."
+        },
+        {
+            id: "52",
+            name: "Block 52",
+            type: "health",
+            coords: [[14.107354, 38.709071], [14.107567, 38.709045], [14.107536, 38.708894], [14.107338, 38.708927]],
+            details: "<b>Block 52 Details</b><br>University Clinic."
+        }
+    ]
+};
